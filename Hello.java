8c56407acfbe4f6f5d19962854a656ccf66edd14
Steven Bolle <smbolle@umich.edu>
2026-02-12 21:50:11 -0500

    Added new message to trigger jenkins

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         for(int i=1;i<=5;i++) {
             System.out.println("Hello World..."+i);
         }
+        System.out.println("Bear was here. This is a change to the file.");
     }
 }
